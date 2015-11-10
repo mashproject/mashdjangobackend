1eea6b6a9be0cf1979bd499671d2810b1e4b90a7
Ads7 <ads71993@gmail.com>
2015-11-10 23:08:27 +0530

    added about user

--- users/serializer.py
+++ users/serializer.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 from users.models import User
 
 __author__ = 'aman'
@@ -7,6 +8,6 @@ __author__ = 'aman'
 class UserDetailedSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id','email', 'first_name', 'last_name', 'fb_profile',
-                  'profile_pic', 'department','username','about_user',
-                  'linkedin_profile','twitter_profile','about_me_profile')
+        fields = ('id', 'email', 'first_name', 'last_name', 'fb_profile',
+                  'profile_pic', 'department', 'username', 'about_user',
+                  'linkedin_profile', 'twitter_profile', 'about_me_profile', 'about_user')
