b4c9c11316729c31c4990f92d2b1b81954210eca
Ads7 <ads71993@gmail.com>
2015-12-06 01:48:01 +0530

    updated email sending

--- mail/forms.py
+++ mail/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 
 class SendMailForm(forms.Form):
-    subject = forms.CharField(required=False)
+    subject = forms.CharField()
+    body = forms.CharField(widget=forms.Textarea)
--- mail/views.py
+++ mail/views.py
@@ -1,17 +1,32 @@
+from boto.ses import SESConnection
+
 from rest_framework.views import APIView
 
-from django.core.mail import send_mail
 from django.shortcuts import render
 from mail.forms import SendMailForm
+from mail.models import Mail
+from mash_backend.settings.production import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, DEFAULT_EMAIL_SENDER
 
 
+# todo make async
 class SendMail(APIView):
     def get(self, request):
         return render(request, 'send_mail_form.html',
                       {'send_mail_form': SendMailForm()})
 
     def post(self, request):
-        send_mail('Subject here', 'Here is the message.', 'ads71993@gmail.com',
-                  ['ads71993@gmail.com'], fail_silently=False)
+        mail_form = SendMailForm(request.POST)
+        connection = SESConnection(aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+        if mail_form.is_valid():
+            try:
+                emails = Mail.objects.all().values_list('email', flat=True)
+            except Mail.DoesNotExist:
+                emails = []
+            connection.send_email(DEFAULT_EMAIL_SENDER, mail_form.cleaned_data['subject'],
+                                  mail_form.cleaned_data['body'], emails)
+            send_errors = dict(message='Triggered Func')
+
+        else:
+            send_errors = dict(message=mail_form.errors)
         return render(request, 'send_mail_form.html',
-                      {'send_mail_form': SendMailForm()})
+                      {'send_mail_form': mail_form, 'send_errors': send_errors})
